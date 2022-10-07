135fa785f1c84e11b358ed825b3befaa73d03fdb
Cody Peterson <cody.dkdc2@gmail.com>
2022-10-07 05:40:36 +0000

    minor edit

--- models/ignoreme.py
+++ models/ignoreme.py
@@ -1,8 +1,10 @@
 # see the ../.dbtignore in the root of the repo -- this file is ignored!
 # use this is you have `.py` files in models/ that aren't models, for some reason
 
+
 def main():
     print("I'm ignored by dbt!")
+
 
 if __name__ == "__main__":
     # call the main function
