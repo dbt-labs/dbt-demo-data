34f6d79eacb0313972880fa231442028ec0b08ef
Jeremy Cohen <jeremy@dbtlabs.com>
2022-10-12 19:23:07 +0200

    Refactor unfuzzing SQL

--- models/challenges/sql/unfuzz_sql.sql
+++ models/challenges/sql/unfuzz_sql.sql
@@ -1,100 +1,59 @@
 /*
-    TODO: document logic
+    For each order, we will compute Levenshtein ratio (required edits / length of string)
+    between that order's customer name, and every known customer name.
+    Then, we'll take the highest ratio (= closest match).
 */
 
-with customer_names as (
+with customers as (
 
-        -- full demo dataset
-        select
-            customer_id as id,
-            customer_name as name
-        
-        from {{ ref('int_customers') }}
+    -- actual customer names
+    select * from {{ ref('int_customers') }}
         
 ),
 
 orders as (
 
-    select
-        *
-    from {{ ref('int_orders') }}
-),
-
-
-order_names as (
-
-    select
-
-        order_id as id,
-        customer_name as name
+    -- orders with erroneous customer names
+    select * from {{ ref('int_orders') }}
     
-    from orders
-    -- limit this for quicker development
-    --limit 100 offset 100
-
 ),
 
 cross_joined as (
 
     select
     
-        a.id as id_a,
-        a.name as name_a,
-        b.id as id_b,
-        b.name as name_b,
+        orders.order_id,
+        orders.customer_name,
+        customers.customer_id as customer_id_match,
+        customers.customer_name as customer_name_match,
     
         -- https://docs.snowflake.com/en/sql-reference/functions/editdistance.html
-        editdistance(a.name, b.name) as levenshtein_distance,
-        greatest(len(a.name), len(b.name)) as max_length,
+        editdistance(lower(orders.customer_name), lower(customers.customer_name)) as levenshtein_distance,
+        greatest(len(orders.customer_name), len(customers.customer_name)) as max_length,
         -- https://stackoverflow.com/questions/14260126/how-python-levenshtein-ratio-is-computed
         (1 - levenshtein_distance/max_length) as levenshtein_ratio
     
-    from order_names a
-    cross join customer_names b
-
-),
-
-matches as (
-
-    select 
-        id_a,
-        name_a, 
-        id_b, 
-        name_b,
-        levenshtein_ratio as match_likelihood
-    from cross_joined
-    -- bump this number for fewer positive matches (both true positives & false positives)
-    -- i.e. the higher the ratio, the more specific & the less sensitive
-    where levenshtein_ratio >= 0.50
-    order by id_a, match_likelihood desc
-
-),
-
-final as (
-
-    select
-        orders.*,
-        matches.id_b as customer_id_match,
-        matches.name_b as customer_name_match,
-        matches.match_likelihood as match_likelihood
     from orders
-    left join matches 
-    on orders.order_id = matches.id_a
-
+    cross join customers
+    -- don't even consider comparisons that require changing more than half the characters
+    where levenshtein_ratio > 0.50
 
 ),
 
-final_filtered as(
-    
+filtered as (
+
     select
-        *
-    from (
-        select
-            *,
-            row_number() over (partition by order_id order by match_likelihood desc) as rn
-        from final
-    )
-    where rn = 1
+    
+        order_id,
+        customer_name,
+        customer_id_match,
+        customer_name_match,
+        levenshtein_ratio
+    
+    from cross_joined
+    -- in case of multiple matches, take the best match
+    qualify row_number() over (partition by order_id order by levenshtein_ratio desc) = 1
+
 )
 
-select * from final_filtered
+select * from filtered
